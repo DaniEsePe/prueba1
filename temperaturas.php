5917c531cfb9ece4277c8e9a2802fd675db97969
Daniel <danisacristanperez@gmail.com>
2017-03-17 11:36:17 +0100

    Add files via upload

---
+++ temperaturas.php
@@ -0,0 +1,40 @@
+<?php
+	header('content-type: text/html; charset=UTF-8');
+	
+	if($_REQUEST['id']==1){
+		echo"<b>Enero:</b> 6.23";
+	}
+	if($_REQUEST['id']==2){
+		echo"<b>Febrero: </b>7.12";
+	}if($_REQUEST['id']==3){
+		echo"<b>Marzo: </b>9.35";
+	}
+	if($_REQUEST['id']==4){
+		echo"<b>Abril: </b>10.10";
+	}
+	if($_REQUEST['id']==5){
+		echo"<b>Mayo: </b>12.23";
+	}
+	if($_REQUEST['id']==6){
+		echo"<b>Junio: </b>18.70";
+	}
+	if($_REQUEST['id']==7){
+		echo"<b>Julio: </b>20.01";
+	}
+	if($_REQUEST['id']==8){
+		echo"<b>Agosto: </b>19.12";
+	}
+	if($_REQUEST['id']==9){
+		echo"<b>Septiembre: </b>16.86";
+	}
+	if($_REQUEST['id']==10){
+		echo"<b>Octubre: </b>12.24";
+	}
+	if($_REQUEST['id']==11){
+		echo"<b>Noviembre: </b>9.13";
+	}
+	if($_REQUEST['id']==12){
+		echo"<b>Diciembre: </b>7.23";
+	}
+
+?>
